0129ef24bbd648c0a6d349e427665d0159589b8c
Zakaria-Tafesh <zakaria.tafesh@gmail.com>
2023-08-22 17:52:30 +0300

    add requirements

--- app.py
+++ app.py
@@ -2,9 +2,11 @@ from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__, template_folder='./templates')
 
+
 @app.route('/')
 def index():
     return render_template('index.html')
+
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -18,9 +20,11 @@ def login():
     else:
         return render_template('login.html')
 
+
 @app.route('/success')
 def success():
     return 'Logged in successfully!'
+
 
 if __name__ == '__main__':
     app.run(debug=True)
